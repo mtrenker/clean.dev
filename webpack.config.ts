216aed5306c4e088c10555c83d5f9c07e161fe29
Martin Trenker <trenker.martin@gmail.com>
2020-03-04 18:20:47 +0100

    Switch to react

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+
+type Config = webpack.Configuration & {
+  devServer: any;
+};
+
+const config: Config = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: [path.resolve(__dirname, "node_modules")],
+        loader: "babel-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle-[hash].js"
+  },
+  plugins: [new HtmlWebpackPlugin(), new CleanWebpackPlugin()],
+  devServer: {
+    open: true
+  }
+};
+
+export default config;
