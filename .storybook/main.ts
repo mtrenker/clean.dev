33d58240ee4d0b19f2adb49250d8bc0ec55ee460
Martin Trenker <trenker.martin@gmail.com>
2020-03-05 10:59:11 +0100

    add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,12 @@
+import webpack from "webpack";
+
+import appConfig from "../webpack.config";
+
+module.exports = {
+  webpackFinal: (storybookConfig: webpack.Configuration): webpack.Configuration => {
+    storybookConfig.module!.rules.push(...appConfig.module!.rules);
+    storybookConfig.resolve!.extensions!.push(".tsx", ".ts")
+    return storybookConfig;
+  },
+  stories: ['../src/**/*.stories.tsx'],
+};
