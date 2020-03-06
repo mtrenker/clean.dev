2148461713523963a62996042553a31fb3b712e5
Martin Trenker <trenker.martin@gmail.com>
2020-03-06 09:51:31 +0100

    add some maintenance text

--- src/pages/Frontpage.tsx
+++ src/pages/Frontpage.tsx
@@ -1,21 +1,9 @@
 import React, { FC } from "react";
 import Helmet from "react-helmet";
+
 import { Header } from "../components/layout/Header";
+import { Grid } from "../components/layout/Grid";
 
 export const Frontpage: FC = () => (
-  <>
-    <Header>
-      <Helmet>
-        <title>Frontpage</title>
-      </Helmet>
-    </Header>
-    <main>
-      <h1> Welcome</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Animi maxime rerum, optio veniam dolore impedit mollitia facere unde nobis.
-        Nam voluptate maxime enim vero numquam repellendus magnam rem hic ipsum?
-      </p>
-    </main>
-  </>
+  <p>The site is currently down for maintenance but will be up again shortly.</p>
 )
