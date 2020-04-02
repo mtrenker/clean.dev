b433202fed44b21ca7e9fea25ed86e3a555ba485
Martin Trenker <trenker.martin@gmail.com>
2020-04-02 12:22:20 +0200

    add cloudfront distribution

--- cdk/stacks/FrontStack.ts
+++ cdk/stacks/FrontStack.ts
@@ -1,8 +1,13 @@
 import {
-  Stack, StackProps, App, SecretValue,
+  Stack, StackProps, App, SecretValue, CfnOutput,
 } from '@aws-cdk/core';
 import { Certificate } from '@aws-cdk/aws-certificatemanager';
-import { HostedZone } from '@aws-cdk/aws-route53';
+import {
+  ARecordProps, ARecord, AaaaRecord, RecordTarget, AaaaRecordProps, HostedZone,
+} from '@aws-cdk/aws-route53';
+import { CloudFrontTarget } from '@aws-cdk/aws-route53-targets';
+import { CloudFrontWebDistribution, ViewerCertificate } from '@aws-cdk/aws-cloudfront';
+
 import { GitHubBuild } from '../constructs/GitHubBuild';
 
 export class FrontStack extends Stack {
@@ -17,21 +22,39 @@ export class FrontStack extends Stack {
       owner: 'mtrenker',
       repo: 'clean.dev',
     });
-  }
 
-  // old() {
-  //   new StaticSite(this, 'CleanDevFront', {
-  //     pipelineConfig: {
-  //       branch: 'master',
-  //       owner: 'mtrenker',
-  //       repo: 'clean.dev',
-  //       oauthToken: SecretValue.secretsManager('clean/github'),
-  //     },
-  //     cloudFrontConfig: {
-  //       alias: 'clean.dev',
-  //       certificate,
-  //       zone: HostedZone.fromLookup(this, 'HostedZone', { domainName: 'clean.dev' }),
-  //     },
-  //   });
-  // }
+    const viewerCertificate = ViewerCertificate.fromAcmCertificate(certificate, {
+      aliases: ['clean.dev'],
+    });
+
+    const zone = HostedZone.fromLookup(this, 'HostedZone', { domainName: 'clean.dev' });
+
+    const cloudFrontDistribution = new CloudFrontWebDistribution(this, 'Distribution', {
+      originConfigs: [{
+        s3OriginSource: {
+          s3BucketSource: build.siteBucket,
+        },
+        behaviors: [{
+          isDefaultBehavior: true,
+        }],
+      }],
+      viewerCertificate,
+    });
+
+    const recordProps: ARecordProps | AaaaRecordProps = {
+      target: RecordTarget.fromAlias(new CloudFrontTarget(cloudFrontDistribution)),
+      zone,
+    };
+
+    new ARecord(this, 'CloudFrontARecord', recordProps);
+    new AaaaRecord(this, 'CloudFrontAaaaRecord', recordProps);
+
+    new CfnOutput(this, 'SiteURL', {
+      value: cloudFrontDistribution.domainName,
+    });
+
+    new CfnOutput(this, 'StorybookURL', {
+      value: build.storybookBucket.bucketWebsiteUrl,
+    });
+  }
 }
