062f3d24726e9c52305da3fc225ed8cccfffdff3
Martin Trenker <trenker.martin@gmail.com>
2020-04-22 09:21:02 +0200

    use original gql schema

--- src/graphql/hooks.tsx
+++ src/graphql/hooks.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import * as ApolloReactCommon from '@apollo/react-common';
 import * as ApolloReactComponents from '@apollo/react-components';
 import * as ApolloReactHooks from '@apollo/react-hooks';
-
 export type Maybe<T> = T | null;
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
@@ -13,6 +12,21 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
+  AWSJSON: { [key: string]: any };
+  AWSDate: Date;
+};
+
+
+
+export type PageInput = {
+  slug: Scalars['String'];
+};
+
+export type Page = {
+   __typename?: 'Page';
+  slug: Scalars['String'];
+  title: Scalars['String'];
+  content?: Maybe<Scalars['AWSJSON']>;
 };
 
 export type Project = {
@@ -21,8 +35,8 @@ export type Project = {
   client: Scalars['String'];
   industry: Scalars['String'];
   description: Scalars['String'];
-  startDate: Scalars['String'];
-  endDate: Scalars['String'];
+  startDate: Scalars['AWSDate'];
+  endDate: Scalars['AWSDate'];
   methodologies: Array<Scalars['String']>;
   technologies: Array<Scalars['String']>;
 };
@@ -30,7 +44,26 @@ export type Project = {
 export type Query = {
    __typename?: 'Query';
   projects: Array<Project>;
+  page?: Maybe<Page>;
 };
+
+
+export type QueryPageArgs = {
+  input: PageInput;
+};
+
+export type GetPageQueryVariables = {
+  input: PageInput;
+};
+
+
+export type GetPageQuery = (
+  { __typename?: 'Query' }
+  & { page?: Maybe<(
+    { __typename?: 'Page' }
+    & Pick<Page, 'slug' | 'title' | 'content'>
+  )> }
+);
 
 export type GetProjectsQueryVariables = {};
 
@@ -40,10 +73,51 @@ export type GetProjectsQuery = (
   & { projects: Array<(
     { __typename?: 'Project' }
     & Pick<Project, 'client' | 'industry' | 'description' | 'startDate' | 'endDate' | 'methodologies' | 'technologies'>
-  )>; }
+  )> }
 );
 
 
+export const GetPageDocument = gql`
+    query getPage($input: PageInput!) {
+  page(input: $input) {
+    slug
+    title
+    content
+  }
+}
+    `;
+export type GetPageComponentProps = Omit<ApolloReactComponents.QueryComponentOptions<GetPageQuery, GetPageQueryVariables>, 'query'> & ({ variables: GetPageQueryVariables; skip?: boolean; } | { skip: boolean; });
+
+    export const GetPageComponent = (props: GetPageComponentProps) => (
+      <ApolloReactComponents.Query<GetPageQuery, GetPageQueryVariables> query={GetPageDocument} {...props} />
+    );
+    
+
+/**
+ * __useGetPageQuery__
+ *
+ * To run a query within a React component, call `useGetPageQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetPageQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetPageQuery({
+ *   variables: {
+ *      input: // value for 'input'
+ *   },
+ * });
+ */
+export function useGetPageQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<GetPageQuery, GetPageQueryVariables>) {
+        return ApolloReactHooks.useQuery<GetPageQuery, GetPageQueryVariables>(GetPageDocument, baseOptions);
+      }
+export function useGetPageLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<GetPageQuery, GetPageQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<GetPageQuery, GetPageQueryVariables>(GetPageDocument, baseOptions);
+        }
+export type GetPageQueryHookResult = ReturnType<typeof useGetPageQuery>;
+export type GetPageLazyQueryHookResult = ReturnType<typeof useGetPageLazyQuery>;
+export type GetPageQueryResult = ApolloReactCommon.QueryResult<GetPageQuery, GetPageQueryVariables>;
 export const GetProjectsDocument = gql`
     query getProjects {
   projects {
@@ -59,10 +133,10 @@ export const GetProjectsDocument = gql`
     `;
 export type GetProjectsComponentProps = Omit<ApolloReactComponents.QueryComponentOptions<GetProjectsQuery, GetProjectsQueryVariables>, 'query'>;
 
-export const GetProjectsComponent = (props: GetProjectsComponentProps) => (
-  <ApolloReactComponents.Query<GetProjectsQuery, GetProjectsQueryVariables> query={GetProjectsDocument} {...props} />
-);
-
+    export const GetProjectsComponent = (props: GetProjectsComponentProps) => (
+      <ApolloReactComponents.Query<GetProjectsQuery, GetProjectsQueryVariables> query={GetProjectsDocument} {...props} />
+    );
+    
 
 /**
  * __useGetProjectsQuery__
@@ -80,11 +154,11 @@ export const GetProjectsComponent = (props: GetProjectsComponentProps) => (
  * });
  */
 export function useGetProjectsQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<GetProjectsQuery, GetProjectsQueryVariables>) {
-  return ApolloReactHooks.useQuery<GetProjectsQuery, GetProjectsQueryVariables>(GetProjectsDocument, baseOptions);
-}
+        return ApolloReactHooks.useQuery<GetProjectsQuery, GetProjectsQueryVariables>(GetProjectsDocument, baseOptions);
+      }
 export function useGetProjectsLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<GetProjectsQuery, GetProjectsQueryVariables>) {
-  return ApolloReactHooks.useLazyQuery<GetProjectsQuery, GetProjectsQueryVariables>(GetProjectsDocument, baseOptions);
-}
+          return ApolloReactHooks.useLazyQuery<GetProjectsQuery, GetProjectsQueryVariables>(GetProjectsDocument, baseOptions);
+        }
 export type GetProjectsQueryHookResult = ReturnType<typeof useGetProjectsQuery>;
 export type GetProjectsLazyQueryHookResult = ReturnType<typeof useGetProjectsLazyQuery>;
 export type GetProjectsQueryResult = ApolloReactCommon.QueryResult<GetProjectsQuery, GetProjectsQueryVariables>;
