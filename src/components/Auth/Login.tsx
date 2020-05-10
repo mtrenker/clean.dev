79f7da7e89ce75b45962ec53b768cc5f03ae859a
Martin Trenker <trenker.martin@gmail.com>
2020-05-10 13:42:09 +0200

    connect username/password boxes to login logic

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext } from 'react';
+import React, { FC, useContext, useRef } from 'react';
 
 import { signIn, signOut } from '../../lib/auth';
 import { UserContext } from '../../context/UserContext';
@@ -6,12 +6,18 @@ import { UserContext } from '../../context/UserContext';
 export const Login: FC = () => {
   const { user, setUser } = useContext(UserContext);
 
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
   const login = async (): Promise<void> => {
-    const loggedInUser = await signIn('username', 'password');
+    const loggedInUser = await signIn(
+      usernameRef.current?.value ?? '',
+      passwordRef.current?.value ?? '',
+    );
     setUser(loggedInUser);
   };
 
-  const logout = async () => {
+  const logout = async (): Promise<void> => {
     await signOut();
     setUser(null);
   };
@@ -21,15 +27,15 @@ export const Login: FC = () => {
     return (
       <p>
         {user.username}
-        <button type="submit" onClick={() => logout()}>Logout</button>
+        <button type="submit" onClick={logout}>Logout</button>
       </p>
     );
   }
   return (
     <div>
-      <input type="text" />
-      <input type="password" />
-      <button type="submit" onClick={() => login()}>Login</button>
+      <input type="text" ref={usernameRef} />
+      <input type="password" ref={passwordRef} />
+      <button type="submit" onClick={login}>Login</button>
     </div>
   );
 };
