0853efd7b215799716870c69b8063aefb67602c4
Martin Trenker <trenker.martin@gmail.com>
2020-05-16 19:05:34 +0200

    layout updates

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,17 +1,68 @@
 import React, { FC } from 'react';
 import { css } from '@emotion/core';
-import { Container } from './Container';
 
-export const Footer: FC = ({ children }) => {
+import { Container, container } from './Container';
+import { breakPoints } from '../../themes/default';
+
+export const Footer: FC = () => {
   const footer = css`
-    border-top: 1px solid rgba(0, 0, 0, .1);
-    height: 180px;
-    padding: 20px;
+    a {
+      color: #000;
+      text-decoration: none;
+    }
+    ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+    }
+    @media (min-width: ${breakPoints.mobile}) {
+      border-top: 1px solid rgba(0, 0, 0, .1);
+      height: 180px;
+      padding: 20px;
+
+      .css-${container.name} {
+        display: flex;
+        justify-content: space-between;
+        div {
+          flex: 1;
+        }
+      }
+    }
+  `;
+  const contact = css`
+    font-style: normal;
   `;
   return (
     <footer css={footer}>
       <Container>
-        {children}
+        <div>
+          <h4>cleandev</h4>
+          <ul>
+            <li>
+              <a href="/">About</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4>social</h4>
+          <ul>
+            <li>
+              <a href="/">LinkedIn</a>
+            </li>
+            <li>
+              <a href="/">GitHub</a>
+            </li>
+            <li>
+              <a href="/">StackOverflow</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4>get in touch</h4>
+          <address css={contact}>
+            Address
+          </address>
+        </div>
       </Container>
     </footer>
   );
--- src/pages/Track.tsx
+++ src/pages/Track.tsx
@@ -2,11 +2,23 @@ import React, { FC } from 'react';
 
 import { Login } from '../components/auth/Login';
 import { TimeTracker } from '../components/widgets/TimeTracker/TimeTracker';
+import { Header } from '../components/layout/Header';
+import { Heading } from '../components/typography/Heading';
+import { Breadcrumbs } from '../components/layout/Breadcrumbs';
+import { Container } from '../components/layout/Container';
+import { Footer } from '../components/layout/Footer';
 
 export const Track: FC = () => (
-  <div>
-    <h1>Tracking</h1>
-    <Login />
-    <TimeTracker />
-  </div>
+  <>
+    <Header>
+      <Heading as="h4">Landing Page</Heading>
+      <div>
+        <Breadcrumbs />
+      </div>
+    </Header>
+    <Container>
+      <TimeTracker />
+    </Container>
+    <Footer />
+  </>
 );
