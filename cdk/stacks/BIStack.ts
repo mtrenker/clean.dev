c70a633dabbfaf93f8cd939d6abb603d0b028e7e
Martin Trenker <trenker.martin@gmail.com>
2020-05-29 22:18:41 +0200

    update BiStack

--- cdk/stacks/BIStack.ts
+++ cdk/stacks/BIStack.ts
@@ -1,19 +1,24 @@
-import { Stack, App, StackProps } from '@aws-cdk/core';
+import {
+  Stack, App, StackProps, RemovalPolicy,
+} from '@aws-cdk/core';
 import { EventBus } from '@aws-cdk/aws-events';
 import { CfnDeliveryStream } from '@aws-cdk/aws-kinesisfirehose';
 import { Bucket } from '@aws-cdk/aws-s3';
 import { Role, ServicePrincipal } from '@aws-cdk/aws-iam';
+import { Table, AttributeType, BillingMode } from '@aws-cdk/aws-dynamodb';
+import { StringParameter } from '@aws-cdk/aws-ssm';
 
 export class BIStack extends Stack {
-  public readonly eventBus: EventBus;
-
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
-    this.eventBus = new EventBus(this, 'Bus', {
+
+    const eventBus = new EventBus(this, 'Bus', {
       eventBusName: 'CleanBus',
     });
 
-    const eventBucket = new Bucket(this, 'EventBucket');
+    const eventBucket = new Bucket(this, 'EventBucket', {
+      bucketName: 'prod.events.clean.dev',
+    });
 
     const firehoseRole = new Role(this, 'FirehoseRole', {
       assumedBy: new ServicePrincipal('firehose.amazonaws.com'),
@@ -32,6 +37,30 @@ export class BIStack extends Stack {
         compressionFormat: 'GZIP',
         roleArn: firehoseRole.roleArn,
       },
+    });
+
+    const inventory = new Table(this, 'Inventory', {
+      partitionKey: {
+        name: 'pk',
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.RETAIN,
+      tableName: 'prod.inventory.clean.dev',
+    });
+
+    new StringParameter(this, 'cleanDevEventBusName', {
+      stringValue: eventBus.eventBusName,
+      parameterName: 'cleanDevEventBusName',
+    });
+
+    new StringParameter(this, 'cleanDevInventoryName', {
+      stringValue: inventory.tableName,
+      parameterName: 'cleanDevInventoryName',
     });
   }
 }
