229970859f0804fe91c14302c6a00a64651c286d
Martin Trenker <trenker.martin@gmail.com>
2020-05-29 22:19:04 +0200

    add auth cert to CertStack

--- cdk/stacks/CertStack.ts
+++ cdk/stacks/CertStack.ts
@@ -1,10 +1,8 @@
 import { Stack, App, StackProps } from '@aws-cdk/core';
-import { Certificate, DnsValidatedCertificate } from '@aws-cdk/aws-certificatemanager';
+import { DnsValidatedCertificate } from '@aws-cdk/aws-certificatemanager';
 import { HostedZone } from '@aws-cdk/aws-route53';
 
 export class CertStack extends Stack {
-  public readonly siteCert: Certificate;
-
   constructor(scope: App, id: string, props: StackProps) {
     super(scope, id, props);
 
@@ -13,8 +11,13 @@ export class CertStack extends Stack {
       privateZone: false,
     });
 
-    this.siteCert = new DnsValidatedCertificate(this, 'TestCertificate', {
+    new DnsValidatedCertificate(this, 'CleanDevCert', {
       domainName: 'clean.dev',
+      hostedZone,
+    });
+
+    new DnsValidatedCertificate(this, 'CleanDevAuthCert', {
+      domainName: 'auth.clean.dev',
       hostedZone,
     });
   }
