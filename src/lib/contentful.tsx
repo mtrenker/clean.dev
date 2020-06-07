a663e47f238d25f799551be7394b91e649c576a3
Martin Trenker <martin@clean.dev>
2020-06-07 15:19:31 +0200

    fix import path for change-password

--- src/lib/contentful.tsx
+++ src/lib/contentful.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { RenderNode } from '@contentful/rich-text-react-renderer';
-import { ChangePassword } from '../components/widgets/TimeTracker/auth/ChangePassword';
+import { ChangePassword } from '../components/widgets/auth/ChangePassword';
 
 export const mapWidgets = (): RenderNode => ({
   'change-password': () => <ChangePassword />,
