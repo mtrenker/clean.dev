792f61c851fcc1efda2656d9150775413a78e3a5
Martin Trenker <martin@clean.dev>
2020-06-27 13:38:47 +0200

    upgrade to storybook 6 beta

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,47 +1,5 @@
-import path from "path";
-import webpack from "webpack";
-import merge from "webpack-merge";
-
-import appConfig from "../webpack.config";
-
 module.exports = {
-  webpackFinal: (storybookConfig: webpack.Configuration): webpack.Configuration => {
-
-    return merge(storybookConfig, {
-      module: {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            exclude: [path.resolve(__dirname, 'node_modules')],
-            loader: 'babel-loader',
-          }, {
-            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'fonts/',
-                },
-              },
-            ],
-          }, {
-            test: /\.(png|jpg)$/i,
-            loader: 'responsive-loader',
-            options: {
-              // eslint-disable-next-line global-require
-              adapter: require('responsive-loader/sharp'),
-              sizes: [300, 600, 1200, 2000],
-              placeholder: true,
-              placeholderSize: 50,
-            },
-          },
-        ]
-      },
-      resolve: appConfig.resolve
-    });
-  },
-  stories: ['../src/**/*.stories.(tsx|mdx)'],
+  stories: ['../src/**/*.stories.@(tsx|mdx)'],
   addons: [
     '@storybook/addon-actions/register',
     '@storybook/addon-knobs/register',
@@ -52,7 +10,6 @@ module.exports = {
       name: '@storybook/addon-storysource',
       options: {
         parser: "typescript",
-
       }
     }
   ]
