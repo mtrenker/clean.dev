dac62f91046090171ce21bb1c0e7b7b73ee3a635
Martin Trenker <martin@clean.dev>
2020-06-28 20:44:32 +0200

    design system changes

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -4,6 +4,60 @@ export interface Theme {
   };
 }
 
+interface Color {
+  title: string;
+  subtitle: string;
+  rgb: number[];
+}
+
+interface Colormap {
+  [category: string]: {
+    [name: string]: string;
+  }
+}
+
+export const palette: Color[] = [{
+  title: 'Contrast',
+  subtitle: 'Raisin Black',
+  rgb: [35, 31, 32],
+}, {
+  title: 'Attention',
+  subtitle: 'International Orange Golden Gate Bridge',
+  rgb: [187, 68, 48],
+}, {
+  title: 'Primary',
+  subtitle: 'Middle Blue',
+  rgb: [126, 189, 194],
+}, {
+  title: 'Secondary',
+  subtitle: 'Medium Champagne',
+  rgb: [243, 223, 162],
+}, {
+  title: 'Background',
+  subtitle: 'Linen',
+  rgb: [239, 230, 221],
+}];
+
+const colorsGenerator = (colors: Color[]): Colormap => {
+  const map: Colormap = {
+    primary: {},
+    secondary: {},
+    attention: {},
+    contrast: {},
+    background: {},
+  };
+  const steps = [30, 60, 80, 100];
+  colors.forEach((color) => {
+    steps.forEach((step) => {
+      const suffix = step < 100 ? `${step}` : '';
+      const alpha = step < 100 ? `.${step}` : 1;
+      const title = color.title.toLowerCase();
+      map[title][`${title}${suffix}`] = `rgba(${color.rgb[0]}, ${color.rgb[1]}, ${color.rgb[2]}, ${alpha})`;
+    });
+  });
+  return map;
+};
+
 export const breakPoints = {
   mobile: '576px',
   tablet: '768px',
@@ -12,8 +66,5 @@ export const breakPoints = {
 };
 
 export const theme = {
-  colors: {
-    primary: 'hotpink',
-    background: '#FFFFFF',
-  },
+  colors: colorsGenerator(palette),
 };
