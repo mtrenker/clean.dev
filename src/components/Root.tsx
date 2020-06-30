9776934867ffce3bd7a3dccaf6f6bb3762a72779
Martin Trenker <martin@clean.dev>
2020-06-30 15:24:40 +0200

    remove theme for now

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,11 +1,9 @@
 import React, { FC, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { ThemeProvider } from 'emotion-theming';
 
 import { Page } from '../pages/Page';
 import { GlobalStyle } from './GlobalStyle';
-import { theme } from '../themes/default';
 import { client } from '../lib/graphql';
 import { UserContext } from '../context/UserContext';
 import { CleanUser, getUser, getCleanUser } from '../lib/auth';
@@ -27,16 +25,14 @@ export const Root: FC = () => {
   return (
     <UserContext.Provider value={{ user, setUser, refreshUser }}>
       <ApolloProvider client={client}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Router>
-            <Switch>
-              <Route path="/">
-                <Page />
-              </Route>
-            </Switch>
-          </Router>
-        </ThemeProvider>
+        <GlobalStyle />
+        <Router>
+          <Switch>
+            <Route path="/">
+              <Page />
+            </Route>
+          </Switch>
+        </Router>
       </ApolloProvider>
     </UserContext.Provider>
   );
