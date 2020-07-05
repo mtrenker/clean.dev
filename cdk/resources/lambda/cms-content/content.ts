d9ef4e1cde67534f6f07e244e10e7a439375f5ce
Martin Trenker <martin@clean.dev>
2020-07-05 10:57:31 +0200

    refactor content lambda and add post type

--- cdk/resources/lambda/cms-content/content.ts
+++ cdk/resources/lambda/cms-content/content.ts
@@ -1,7 +1,51 @@
 import { DynamoDB } from 'aws-sdk';
 import { SNSHandler } from 'aws-lambda';
 import { BLOCKS } from '@contentful/rich-text-types';
-import { createClient } from 'contentful';
+import { createClient, Entry } from 'contentful';
+
+interface Author {
+  name: string;
+  avarar: string;
+  page: CmsLink;
+}
+
+interface Body<ContentId, Fields> {
+  sys: {
+    id: string;
+    type: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedCounter: number,
+    version: number,
+    contentType: {
+      sys: {
+        type: string;
+        linkType: string;
+        id: ContentId;
+      }
+    };
+  }
+  fields: Fields
+}
+
+interface PageFields {
+  title: Localized<string>;
+  slug: Localized<string>;
+  content: Localized<CmsNode>;
+}
+
+type Page = Body<'page', PageFields>
+
+interface PostFields {
+  title: Localized<string>;
+  author: Localized<CmsLink>;
+  intro: Localized<CmsNode>;
+  content: Localized<CmsNode>;
+}
+
+type Post = Body<'post', PostFields>
+
+type EventBody = Page | Post;
 
 interface CmsNode {
   nodeType: string;
@@ -17,9 +61,6 @@ interface CmsNode {
   content: CmsNode[]
 }
 
-interface Component {
-  name: string;
-}
 interface Blueprint {
   name: string;
 }
@@ -28,12 +69,12 @@ interface Localized<T> {
   [language: string]: T
 }
 
-interface CmsPage {
-  fields: {
-    title: Localized<string>;
-    slug: Localized<string>;
-    content: Localized<CmsNode>;
-  };
+interface CmsLink {
+  sys: {
+    type: string;
+    linkType: string;
+    id: string;
+  }
 }
 
 const TABLE_NAME = process.env.TABLE_NAME ?? '';
@@ -43,46 +84,76 @@ const client = new DynamoDB.DocumentClient({
   region: REGION,
 });
 
-async function saveToDynamoDb(id: string, page: CmsPage): Promise<void> {
-  await client.update({
+const contentfulClient = createClient({
+  accessToken: process.env.CONTENTFUL_API_KEY || '',
+  space: process.env.CONTENTFUL_SPACE_ID || '',
+});
+
+async function putDocument(id: string, document: Record<string, unknown>) {
+  await client.put({
     TableName: TABLE_NAME,
-    Key: {
+    Item: {
       pk: id,
       id,
+      ...document,
     },
-    UpdateExpression: 'set #title= :title, #content = :content, #slug = :slug',
-    ExpressionAttributeNames: {
-      '#title': 'title',
-      '#slug': 'slug',
-      '#content': 'content',
-    },
-    ExpressionAttributeValues: {
-      ':content': JSON.stringify(page.fields.content['en-US']),
-      ':title': page.fields.title['en-US'],
-      ':slug': page.fields.slug['en-US'],
-    },
+
   }).promise();
 }
 
-export const handler: SNSHandler = async (event) => {
-  const { Message } = event.Records[0].Sns;
-  const page = JSON.parse(Buffer.from(Message, 'base64').toString()) as CmsPage;
-  const { slug } = page.fields;
-  const id = `page-${slug['en-US']}`;
+async function resolveContentfulAuthor(authorLink: CmsLink): Promise<Entry<Author>> {
+  const author = await contentfulClient.getEntry<Author>(authorLink.sys.id);
+  return author;
+}
 
-  const contentfulClient = createClient({
-    accessToken: process.env.CONTENTFUL_API_KEY || '',
-    space: process.env.CONTENTFUL_SPACE_ID || '',
-  });
-
-  const promises = page.fields.content['en-US'].content.map(async (node, index) => {
+async function resolveContentfulNodes(content: CmsNode) {
+  const promises = content.content.map(async (node: CmsNode) => {
     if (node.nodeType === BLOCKS.EMBEDDED_ENTRY) {
-      const entry = await contentfulClient.getEntry<Component|Blueprint>(node.data.target?.sys.id ?? 'no-id');
-      page.fields.content['en-US'].content[index] = { nodeType: entry.fields.name, data: {}, content: [] };
+      const entry = await contentfulClient.getEntry<Blueprint>(node.data.target?.sys.id ?? 'no-id');
+      return { nodeType: entry.fields.name, data: {}, content: [] };
     }
+    return new Promise<CmsNode>((resolve) => resolve(node));
   });
+  const resolvedContent = await Promise.all(promises);
+  console.log('UNRESOLVED', content);
+  console.log('RESOLVED', resolvedContent);
 
-  await Promise.all(promises);
+  return resolvedContent;
+}
 
-  await saveToDynamoDb(id, page);
+export const handler: SNSHandler = async (event) => {
+  const lang = 'en-US';
+  const { Message } = event.Records[0].Sns;
+  const body = JSON.parse(Buffer.from(Message, 'base64').toString()) as EventBody;
+
+  switch (body.sys.contentType.sys.id) {
+    case 'post': {
+      const post = body.fields as PostFields;
+      const id = `post-${body.sys.id}`;
+      const content = await resolveContentfulNodes(post.content[lang]);
+      const author = await resolveContentfulAuthor(post.author[lang]);
+      const postDocument = {
+        title: post.title[lang],
+        author: author.fields,
+        intro: JSON.stringify(post.intro[lang]),
+        content: JSON.stringify(content),
+      };
+      await putDocument(id, postDocument);
+      break;
+    }
+    case 'page': {
+      const page = body.fields as PageFields;
+      const id = `page-${page.slug[lang]}`;
+      const content = await resolveContentfulNodes(page.content[lang]);
+      const pageDocument = {
+        title: page.title[lang],
+        slug: page.slug[lang],
+        content: JSON.stringify(content),
+      };
+      await putDocument(id, pageDocument);
+      break;
+    }
+    default:
+      break;
+  }
 };
