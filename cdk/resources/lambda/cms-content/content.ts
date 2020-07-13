ee8bac7c064db7a93063fcca2e6504d278bd5397
Martin Trenker <martin@clean.dev>
2020-07-13 18:13:32 +0200

    add image

--- cdk/resources/lambda/cms-content/content.ts
+++ cdk/resources/lambda/cms-content/content.ts
@@ -1,11 +1,26 @@
 import { DynamoDB } from 'aws-sdk';
 import { SNSHandler } from 'aws-lambda';
 import { BLOCKS } from '@contentful/rich-text-types';
-import { createClient, Entry } from 'contentful';
+import { createClient, Asset } from 'contentful';
+
+interface Image {
+  title: string;
+  description: string;
+  width: number;
+  height: number;
+  size: number;
+  url: string;
+}
+
+interface CmsAuthor {
+  name: string;
+  avatar: Asset;
+  page: CmsLink;
+}
 
 interface Author {
   name: string;
-  avarar: string;
+  avatar: Image;
   page: CmsLink;
 }
 
@@ -40,6 +55,7 @@ interface PostFields {
   title: Localized<string>;
   slug: Localized<string>;
   author: Localized<CmsLink>;
+  heroImage: Localized<CmsLink>;
   intro: Localized<CmsNode>;
   content: Localized<CmsNode>;
 }
@@ -102,9 +118,30 @@ async function putDocument(pk: string, id: string, document: Record<string, unkn
   }).promise();
 }
 
-async function resolveContentfulAuthor(authorLink: CmsLink): Promise<Entry<Author>> {
-  const author = await contentfulClient.getEntry<Author>(authorLink.sys.id);
-  return author;
+function mapAsset(asset: Asset): Image {
+  const { fields: { file, title, description } } = asset;
+  return {
+    title,
+    description,
+    height: file.details.image.height,
+    width: file.details.image.width,
+    size: file.details.size,
+    url: file.url,
+  };
+}
+
+async function resolveAsset(link: CmsLink): Promise<Asset> {
+  const asset = await contentfulClient.getAsset(link.sys.id);
+  return asset;
+}
+
+async function resolveLink(link: CmsLink): Promise<Author> {
+  const { fields: { avatar, name, page } } = await contentfulClient.getEntry<CmsAuthor>(link.sys.id);
+  return {
+    avatar: mapAsset(avatar),
+    name,
+    page,
+  };
 }
 
 async function resolveContentfulNodes(node: CmsNode): Promise<CmsNode> {
@@ -134,13 +171,15 @@ export const handler: SNSHandler = async (event) => {
       const id = pk;
       const intro = await resolveContentfulNodes(post.intro[lang]);
       const content = await resolveContentfulNodes(post.content[lang]);
-      const author = await resolveContentfulAuthor(post.author[lang]);
+      const author = await resolveLink(post.author[lang]);
+      const heroAsset = await resolveAsset(post.heroImage[lang]);
       const postDocument = {
         title: post.title[lang],
         slug: post.slug[lang],
+        author,
+        heroImage: mapAsset(heroAsset),
         intro: JSON.stringify(intro),
         content: JSON.stringify(content),
-        author: author.fields,
       };
       await putDocument(pk, id, postDocument);
       break;
