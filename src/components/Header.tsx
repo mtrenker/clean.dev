5895a92565b7f2a07eaecef07844086ce3e99816
Martin Trenker <martin@clean.dev>
2020-07-20 07:44:42 +0200

    add mobile navigation
    closes #5

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,75 +1,129 @@
-import React, { FC } from 'react';
-import { css } from '@emotion/core';
+import React, { FC, useState } from 'react';
+import { css, keyframes } from '@emotion/core';
 
 import { Link } from 'react-router-dom';
 import { Container, container } from './Container';
 import { breakPoints } from '../themes/default';
 import { Icon } from './Icon';
 
-const onMenuClick = (e: React.MouseEvent<SVGElement>) => {
-  console.log(e);
-};
+const slideIn = keyframes`
+  from {
+    top: -50px;
+    z-index: -1;
+  }
+  99% {
+    top: 0;
+    z-index: -1;
+  }
+  to {
+    z-index: 0;
+  }
+`;
+const slideOut = keyframes`
+  from {
+    top: 0;
+  }
 
-export const Header: FC = ({ children }) => {
-  const headerCss = css`
-    @media (min-width: ${breakPoints.mobile}) {
-      display: grid;
-      grid-template:
-        "topbar topbar" auto
-        "infobar infobar" max-content
-        / 1fr 1fr;
-      background-color: #F5F5F5;
+  to {
+    top: -50px;
+    z-index: -1;
+  }
+`;
+
+const headerCss = css`
+  @media (min-width: ${breakPoints.mobile}) {
+    display: grid;
+    position: relative;
+    grid-template:
+      "topbar topbar" auto
+      / 1fr 1fr;
+    background-color: #F5F5F5;
+  }
+`;
+
+const topbarCss = css`
+  background-color: white ;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, .1);
+  grid-area: topbar;
+
+  .css-${container.name} {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const logoCss = css`
+  flex: 1;
+  font-weight: bold;
+  span {
+    font-weight: lighter;
+    font-family: consolas;
+  }
+`;
+
+const navCss = css`
+  ul {
+    display: none;
+  }
+  svg {
+    cursor: pointer;
+  }
+  @media (min-width: ${breakPoints.mobile}) {
+    svg {
+      display: none;
     }
-  `;
-
-  const topbarCss = css`
-    background-color: white ;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, .1);
-    grid-area: topbar;
-
-    .css-${container.name} {
-      display: flex;
-      align-items: center;
-    }
-  `;
-
-  const infobarCss = css`
-    grid-area: infobar;
-    .css-${container.name} {
-      height: 100%;
+    display: block;
+    flex: 1;
+    ul {
       display: flex;
       justify-content: space-between;
-      align-items: center;
-    }
-  `;
-
-  const logoCss = css`
-    flex: 1;
-    font-weight: bold;
-    span {
-      font-weight: lighter;
-      font-family: consolas;
-    }
-  `;
-
-  const navCss = css`
-    @media (max-width: ${breakPoints.mobile}) {
-      display: block;
-      flex: 1;
-      ul {
-        display: flex;
-        justify-content: space-between;
-        li {
-          display: block;
-          a {
-            text-decoration: none;
-            color: #000;
-          }
+      li {
+        display: block;
+        a {
+          text-decoration: none;
+          color: #000;
         }
       }
     }
-  `;
+  }
+`;
 
+const mobileNavCss = css`
+  display: none;
+  @media (max-width: ${breakPoints.mobile}) {
+    display: block;
+    position: relative;
+    &.hide {
+      top: -50px;
+      z-index: -1;
+      animation: ${slideOut} .5s ease-in;
+    }
+    &.show {
+      animation: ${slideIn} .5s ease-in;
+    }
+    ul {
+      display: flex;
+      justify-content: space-between;
+      margin: 0;
+      padding: 0 1rem;
+      li {
+        list-style: none;
+        display: block;
+        a {
+          text-decoration: none;
+          color: #000;
+        }
+      }
+    }
+  }
+`;
+
+export const Header: FC = () => {
+  const [showMobileNav, setShowMobileNav] = useState<boolean>(false);
+  const onMenuClick = (e: React.MouseEvent<SVGElement>) => {
+    e.preventDefault();
+    setShowMobileNav(!showMobileNav);
+  };
   return (
     <header css={headerCss}>
       <div css={topbarCss}>
@@ -82,20 +136,15 @@ export const Header: FC = ({ children }) => {
             <Icon icon="bars" onClick={onMenuClick} />
             <ul>
               <li><Link to="/">Home</Link></li>
-              <li><Link to="/blog">Blog</Link></li>
-              <li><Link to="/blog">Blog</Link></li>
-              <li><Link to="/blog">Blog</Link></li>
             </ul>
           </nav>
         </Container>
       </div>
-      { children && (
-      <div css={infobarCss}>
-        <Container>
-          {children}
-        </Container>
+      <div css={mobileNavCss} className={showMobileNav ? 'show' : 'hide'}>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+        </ul>
       </div>
-      )}
     </header>
   );
 };
