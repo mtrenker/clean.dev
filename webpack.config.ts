d4c8cafb61a41ff110ee915c44c67d644f09b6da
Martin Trenker <martin@clean.dev>
2020-07-20 16:29:28 +0200

    remove svg from file exporter
    closes #7

--- webpack.config.ts
+++ webpack.config.ts
@@ -29,7 +29,7 @@ const config: Config = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       }, {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'file-loader',
