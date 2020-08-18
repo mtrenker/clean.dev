87c8823f09e77b3683b4b4c24fd6215d166f9ffb
Martin Trenker <martin@clean.dev>
2020-08-18 08:51:51 +0200

    set a11y addon to run manually

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,11 +2,18 @@ import React, { FC } from "react";
 import { addDecorator } from "@storybook/react"
 import StoryRouter from 'storybook-react-router';
 import { ThemeProvider, } from "emotion-theming";
+import {withA11y} from "@storybook/addon-a11y"
 
 import { theme } from "../src/themes/default"
 import { GlobalStyle } from "../src/components/GlobalStyle";
 
 export const parameters = {
+  a11y: {
+    element: '#root',
+    config: {},
+    options: {},
+    manual: true,
+  },
   options: {
     storySort: {
       method: '',
