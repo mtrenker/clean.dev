7ad71a1aa355184af41f33052a481db6aac674f8
Martin Trenker <martin@clean.dev>
2020-08-22 08:35:10 +0200

    remove container

--- src/components/layout/Header.stories.tsx
+++ src/components/layout/Header.stories.tsx
@@ -1,42 +1,39 @@
 import React, { FC } from 'react';
 
 import { Header } from './Header';
-import { Container } from './Container';
 
 export default { title: 'Components/Header' };
 
 export const Component: FC = () => (
   <>
     <Header />
-    <Container>
-      <h1>Lorem Ipsum</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti itaque a quod incidunt dicta iure assumenda,
-        aut placeat facere at, unde modi. Molestiae rem officiis labore rerum dignissimos voluptas vitae?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti itaque a quod incidunt dicta iure assumenda,
-        aut placeat facere at, unde modi. Molestiae rem officiis labore rerum dignissimos voluptas vitae?
-      </p>
-      <img src="https://picsum.photos/400/250?grayscale" alt="Example pic" />
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti itaque a quod incidunt dicta iure assumenda,
-        aut placeat facere at, unde modi. Molestiae rem officiis labore rerum dignissimos voluptas vitae?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti itaque a quod incidunt dicta iure assumenda,
-        aut placeat facere at, unde modi. Molestiae rem officiis labore rerum dignissimos voluptas vitae?
-      </p>
-      <img src="https://picsum.photos/400/249?grayscale" alt="Example pic" />
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti itaque a quod incidunt dicta iure assumenda,
-        aut placeat facere at, unde modi. Molestiae rem officiis labore rerum dignissimos voluptas vitae?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti itaque a quod incidunt dicta iure assumenda,
-        aut placeat facere at, unde modi. Molestiae rem officiis labore rerum dignissimos voluptas vitae?
-      </p>
-      <img src="https://picsum.photos/400/248?grayscale" alt="Example pic" />
-    </Container>
+    <h1>Lorem Ipsum</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti itaque a quod incidunt dicta iure assumenda,
+      aut placeat facere at, unde modi. Molestiae rem officiis labore rerum dignissimos voluptas vitae?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti itaque a quod incidunt dicta iure assumenda,
+      aut placeat facere at, unde modi. Molestiae rem officiis labore rerum dignissimos voluptas vitae?
+    </p>
+    <img src="https://picsum.photos/400/250?grayscale" alt="Example pic" />
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti itaque a quod incidunt dicta iure assumenda,
+      aut placeat facere at, unde modi. Molestiae rem officiis labore rerum dignissimos voluptas vitae?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti itaque a quod incidunt dicta iure assumenda,
+      aut placeat facere at, unde modi. Molestiae rem officiis labore rerum dignissimos voluptas vitae?
+    </p>
+    <img src="https://picsum.photos/400/249?grayscale" alt="Example pic" />
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti itaque a quod incidunt dicta iure assumenda,
+      aut placeat facere at, unde modi. Molestiae rem officiis labore rerum dignissimos voluptas vitae?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti itaque a quod incidunt dicta iure assumenda,
+      aut placeat facere at, unde modi. Molestiae rem officiis labore rerum dignissimos voluptas vitae?
+    </p>
+    <img src="https://picsum.photos/400/248?grayscale" alt="Example pic" />
   </>
 );
