6e7ea92f29dcb7fed32a08f90edabf66e7e14ee1
Martin Trenker <martin@pacabytes.io>
2020-10-23 07:50:45 +0200

    add BlogApi construct

---
+++ cdk/constructs/Blog/BlogApi.ts
@@ -0,0 +1,53 @@
+import {
+  DynamoDbDataSource, GraphqlApi, GraphqlType, MappingTemplate, ObjectType, ResolvableField,
+} from '@aws-cdk/aws-appsync';
+import { Construct } from '@aws-cdk/core';
+
+interface BlogApiProps {
+  api: GraphqlApi
+  querySource: DynamoDbDataSource
+}
+
+export class BlogApi extends Construct {
+  constructor(scope: Construct, id: string, props: BlogApiProps) {
+    super(scope, id);
+
+    const { api, querySource } = props;
+
+    const requiredString = GraphqlType.string({ isRequired: true });
+
+    const pageType = new ObjectType('Page', {
+      definition: {
+        title: requiredString,
+        slug: requiredString,
+        content: requiredString,
+        layout: requiredString,
+      },
+    });
+
+    api.addQuery('getPage', new ResolvableField({
+      returnType: pageType.attribute(),
+      args: {
+        slug: requiredString,
+      },
+      dataSource: querySource,
+      requestMappingTemplate: MappingTemplate.fromString(`
+        {
+          "version" : "2018-05-29",
+          "operation" : "Query",
+          "index": "GSI1",
+          "query":{
+            "expression": "sk = :slug AND #data = :slug",
+            "expressionNames": {
+              "#data": "data"
+            },
+            "expressionValues": {
+              ":slug": $util.dynamodb.toDynamoDBJson("$ctx.args.slug")
+            }
+          },
+        }
+      `),
+      responseMappingTemplate: MappingTemplate.fromString('$util.toJson($ctx.result.items[0])'),
+    }));
+  }
+}
