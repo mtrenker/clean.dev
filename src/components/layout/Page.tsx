53de65207ff85aac7ae02444950d129957500a6a
Martin Trenker <martin@pacabytes.io>
2020-11-05 18:01:11 +0100

    add close button for mobile menu

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -9,6 +9,7 @@ import { Footer } from './Footer';
 import { mapWidgets } from '../../lib/contentful';
 import { ErrorBoundary } from './ErrorBoundary';
 import { useTheme, css } from '../../lib/style';
+import CloseIcon from '../../assets/icons/cross-circle.svg';
 
 export const Page: FC = () => {
   const [sideMenuOpen, setSideMenuOpen] = useState<boolean>(false);
@@ -17,7 +18,7 @@ export const Page: FC = () => {
   const page = pathname.split('/')[1];
   const { data, error } = useGetPageQuery({ variables: { slug: `/${page}` } });
 
-  const [{ offsetLeft, offsetRight }, set] = useSpring(() => ({ offsetLeft: 0, offsetRight: 0 }));
+  const [{ offsetLeft, offsetRight, opacity }, set] = useSpring(() => ({ offsetLeft: 0, offsetRight: 0, opacity: 1 }));
 
   if (!data) return <p>Loading</p>;
   const document = data?.getPage?.content ?? '';
@@ -33,18 +34,24 @@ export const Page: FC = () => {
     return content;
   };
 
+  const onCloseMenuClick = () => {
+    set({ offsetRight: 0, offsetLeft: 0, opacity: 1 });
+    setSideMenuOpen(false);
+  };
+
   const onHeaderMenuClick = () => {
     setSideMenuOpen(!sideMenuOpen);
     if (sideMenuOpen) {
-      set({ offsetRight: 0, offsetLeft: 0 });
+      set({ offsetRight: 0, offsetLeft: 0, opacity: 1 });
     } else {
-      set({ offsetRight: 275, offsetLeft: -275 });
+      set({ offsetRight: 275, offsetLeft: -275, opacity: 0.5 });
     }
   };
 
   return (
     <div css={pageCss}>
       <nav className="mobile-navigation">
+        <CloseIcon onClick={() => onCloseMenuClick()} />
         <ul>
           <li>1</li>
           <li>2</li>
@@ -53,7 +60,7 @@ export const Page: FC = () => {
           <li>5</li>
         </ul>
       </nav>
-      <animated.main className="page-content" style={{ left: offsetLeft, right: offsetRight }}>
+      <animated.main className="page-content" style={{ left: offsetLeft, right: offsetRight, opacity }}>
         <Header onMenuClick={onHeaderMenuClick} />
         {error && <p>{error.message}</p>}
         <ErrorBoundary>
@@ -72,7 +79,16 @@ const pageCss = css`
     left: 0;
     bottom: 0;
     right: 0;
-    background-color: darkblue;
+    background-color: #000;
+    svg {
+      cursor: pointer;
+      position: absolute;
+      top: 15px;
+      right: 15px;
+      fill: #FFF;
+      width: 32px;
+      height: 32px;
+    }
   }
 
   .page-content {
