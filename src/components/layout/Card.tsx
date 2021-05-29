33c0ba483c96fc843384b5f6e466d2abd0707c2c
Martin Trenker <martin@pacabytes.io>
2021-05-29 08:33:19 +0200

    styling

--- src/components/layout/Card.tsx
+++ src/components/layout/Card.tsx
@@ -8,18 +8,14 @@ export interface CardProps {
   outlined?: boolean;
 }
 
-const cardCss = ({ outlined }: Partial<CardProps>, { card }: Theme) => css`
+const cardCss = ({ card }: Theme) => css`
   box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12);
   border-radius: ${card.border.radius}px;
   overflow: hidden;
-  ${outlined && css`
-    border: 1px solid rgba(0, 0, 0, 0.12);
-    box-shadow: none;
-  `};
 `;
 
 export const Card: FC<CardProps> = ({ children, className, outlined }) => (
-  <article css={cardCss({ outlined })} className={className}>
+  <article css={cardCss} className={className}>
     {children}
   </article>
 );
