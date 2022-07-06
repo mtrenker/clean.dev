79a47adc78e2562e5c2618ac2ad97839728cae79
Martin Trenker <martin@pacabytes.io>
2022-07-06 12:15:39 +0200

    add inventorystack

--- packages/app/lib/app-stage.ts
+++ packages/app/lib/app-stage.ts
@@ -1,23 +1,12 @@
 import { RemovalPolicy, Stage, StageProps } from 'aws-cdk-lib';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
+import { InventoryStack } from './inventory-stack';
 
 export class AppStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
-    const inventoryTable = new Table(this, 'InventoryTable', {
-      partitionKey: {
-        name: 'pk',
-        type: AttributeType.STRING,
-      },
-      sortKey: {
-        name: 'sk',
-        type: AttributeType.STRING,
-      },
-      billingMode: BillingMode.PAY_PER_REQUEST,
-      removalPolicy: RemovalPolicy.RETAIN,
-    })
-
+    new InventoryStack(this, 'InventoryStack')
   }
 }
---
+++ packages/app/lib/inventory-stack.ts
@@ -0,0 +1,22 @@
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+
+export class InventoryStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const inventoryTable = new Table(this, 'InventoryTable', {
+      partitionKey: {
+        name: 'pk',
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'sk',
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
+  }
+}
