b2f67a6efd147342e97a1187874ac06203927bf4
Martin Trenker <martin@pacabytes.io>
2022-07-16 14:11:16 +0200

    layout styling

--- packages/clean-web/components/Layout.stories.tsx
+++ packages/clean-web/components/Layout.stories.tsx
@@ -9,6 +9,12 @@ export default {
   }
 } as Meta<LayoutProps>;
 
-const Template: Story<LayoutProps> = (props) => <Layout {...props}><p>Hello World</p></Layout>;
+const Template: Story<LayoutProps> = (props) => (
+  <Layout {...props}>
+    <div className='mx-auto max-w-screen-2xl p-4'>
+      <p>Hello World</p>
+    </div>
+  </Layout>
+);
 
 export const Default = Template.bind({});
--- packages/clean-web/components/Layout.tsx
+++ packages/clean-web/components/Layout.tsx
@@ -5,33 +5,28 @@ export interface LayoutProps {
 }
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <div className="h-screen dark:bg-slate-800 dark:text-slate-400">
+  <div className="flex h-screen flex-col dark:bg-slate-800 dark:text-slate-400">
 
-    <header className="flex">
-
-      <h1 className="flex-1">Clean Web</h1>
+    <header className="flex h-20 grow-0 items-center border-b border-b-slate-700 p-4">
+      <h1 className="flex-1">clean web</h1>
 
       <nav className="flex-1">
-        <ul className="flex">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/me">
-              Me
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog">
-              Blog
-            </Link>
-          </li>
+        <ul className="flex justify-end gap-4 px-4">
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/me">Me</Link></li>
+          <li><Link href="/blog">Blog</Link></li>
         </ul>
       </nav>
 
     </header>
 
-    {children}
+    <div className="flex-1">
+      {children}
+    </div>
+
+    <footer className="h-40 w-full grow-0 bg-slate-900 p-4">
+      Footer
+    </footer>
 
   </div>
 );
