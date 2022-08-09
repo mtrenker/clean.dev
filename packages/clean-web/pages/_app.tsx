cf070e1d58fdd418433b3bbc0389018f22c47f65
Martin Trenker <martin@pacabytes.io>
2022-08-09 14:45:41 +0200

    add signin mask

--- packages/clean-web/pages/_app.tsx
+++ packages/clean-web/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Auth } from '@aws-amplify/auth';
+
 import { Layout } from '../common/components/Layout';
+import '../styles/globals.css';
+
+const awsConfig = {
+  Auth: {
+    region: process.env.NEXT_PUBLIC_AWS_REGION,
+    userPoolId: process.env.NEXT_PUBLIC_COGNITO_POOL_ID,
+    userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
+  },
+};
+
+Auth.configure(awsConfig);
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
