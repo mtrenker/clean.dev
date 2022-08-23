4df9ff1224fa33320e59f2e35b3161c36ab2e8bc
Martin Trenker <martin@pacabytes.io>
2022-08-23 10:57:21 +0200

    remove nav

--- packages/clean-web/common/components/Layout.tsx
+++ packages/clean-web/common/components/Layout.tsx
@@ -13,17 +13,6 @@ export interface NavItem {
   label: string;
 }
 
-const navItems: NavItem[] = [{
-  label: 'Home',
-  href: '/',
-}, {
-  label: 'Profile',
-  href: '/profile',
-}, {
-  label: 'Contact',
-  href: '/contact',
-}];
-
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   const { user, setUser } = useAuthenticator();
   useEffect(() => {
@@ -45,13 +34,13 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
           <h1 className="flex-1">clean.dev</h1>
           <nav className="w-fit flex-initial">
             <ul className="flex justify-end divide-x divide-slate-400">
-              {navItems.map(({ href, label }) => (
+              {/* {navItems.map(({ href, label }) => (
                 <li className="flex-1 px-4 text-center first:pl-0 last:pr-0" key={href}>
                   <Link href={href} passHref>
                     <a className="block font-medium text-slate-400 hover:text-slate-300" href="/">{label}</a>
                   </Link>
                 </li>
-              ))}
+              ))} */}
               {user ? (
                 <li className="flex-1 px-4 text-center first:pl-0 last:pr-0">
                   <Link href="/signout" passHref>
--- packages/clean-web/pages/index.tsx
+++ packages/clean-web/pages/index.tsx
@@ -1,14 +1,30 @@
+import clsx from 'clsx';
 import type { NextPage } from 'next';
 
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto">
-      <div>
-        <h1 className="text-6xl">
-          Let&apos;s build something together!
-        </h1>
-      </div>
+    <div className={clsx([
+      'h-[calc(100vh-96px)]',
+        'bg-gradient-to-b from-slate-900 to-slate-800',
+      ])}
+    >
+      <main className="container mx-auto px-4">
+        <section>
+          <h1 className="text-4xl">
+            Hi, my name is Martin.
+            I&apos;m a consultant and cloud developer.
+          </h1>
+          <h2 className="text-3xl">
+            I focus on cloud native development, DevOps and cloud security.
+          </h2>
+        </section>
+        <section>
+          <h2 className="text-2xl">
+            What I do
+          </h2>
+        </section>
+      </main>
     </div>
   );
 };
