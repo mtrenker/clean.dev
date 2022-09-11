2b6e16f85ea05fadcca4c94da7e1fbbaac437c20
Martin Trenker <martin@pacabytes.io>
2022-09-11 08:45:10 +0200

    add headers to graphql client

--- packages/clean-web/graphql/client.ts
+++ packages/clean-web/graphql/client.ts
@@ -1,6 +1,26 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { Auth } from '@aws-amplify/auth';
+
+const httpLink = createHttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  try {
+    const token = await Auth.currentSession().then((session) => session.getIdToken().getJwtToken());
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 export const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
