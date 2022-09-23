6c2c632e373ddebfe3c4b59e0190c9ccf3c767d8
Martin Trenker <martin@pacabytes.io>
2022-09-23 18:59:27 +0200

    update dependencies

--- packages/clean-app/jest.config.js
+++ packages/clean-app/jest.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>/lib'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
