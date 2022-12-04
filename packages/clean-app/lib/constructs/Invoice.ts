4007a85258b34927db1901955eac16cea3985735
Martin Trenker <martin@pacabytes.io>
2022-12-04 14:38:13 +0100

    add invoice mutations

--- packages/clean-app/lib/constructs/Invoice.ts
+++ packages/clean-app/lib/constructs/Invoice.ts
@@ -1,14 +1,18 @@
-import { GraphqlApi, GraphqlType, ObjectType } from '@aws-cdk/aws-appsync-alpha';
+import { GraphqlApi, GraphqlType, InputType, MappingTemplate, ObjectType, ResolvableField } from '@aws-cdk/aws-appsync-alpha';
+import { ITable } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 interface InvoiceProps {
   readonly api: GraphqlApi;
+  table: ITable;
 }
 
 export class Invoice extends Construct {
   constructor (scope: Construct, id: string, props: InvoiceProps) {
     super(scope, id);
-    const { api } = props;
+    const { api, table } = props;
+
+    const dataSource = api.addDynamoDbDataSource('InvoiceDataSource', table);
 
     const contactType = new ObjectType('InvoiceContact', {
       definition: {
@@ -52,5 +56,82 @@ export class Invoice extends Construct {
       },
     });
     api.addType(invoiceType);
+
+    const invoiceInputType = new InputType('InvoiceInput', {
+      definition: {
+        vendor: contactType.attribute({ isRequired: true }),
+        client: contactType.attribute({ isRequired: true }),
+        number: GraphqlType.int({ isRequired: true }),
+        date: GraphqlType.awsDate({ isRequired: true }),
+        deliveryDate: GraphqlType.awsDate({ isRequired: true }),
+        dueDate: GraphqlType.awsDate({ isRequired: true }),
+        project: GraphqlType.string({ isRequired: true }),
+        positions: invoicePosition.attribute({ isRequired: true, isList: true }),
+      },
+    });
+    api.addType(invoiceInputType);
+
+    api.addMutation('createInvoice', new ResolvableField({
+      returnType: invoiceType.attribute(),
+      args: {
+        input: invoiceInputType.attribute({ isRequired: true }),
+      },
+      requestMappingTemplate: MappingTemplate.fromString(`
+        {
+          "version": "2018-05-29",
+          "operation": "PutItem",
+          "key": {
+            "pk": $util.dynamodb.toDynamoDBJson($ctx.identity.sub),
+            "sk": $util.dynamodb.toDynamoDBJson("INVOICE#" + $args.input.number)
+          },
+          "attributeValues": $util.dynamodb.toMapValuesJson($util.dynamodb.toMapValues($ctx.args.input))
+        }
+      `),
+    }));
+
+    api.addQuery('getInvoices', new ResolvableField({
+      dataSource,
+      returnType: invoiceType.attribute({ isList: true, isRequiredList: true }),
+      args: {
+        date: GraphqlType.awsDate(),
+      },
+      requestMappingTemplate: MappingTemplate.fromString(`
+        {
+          "version": "2018-05-29",
+          "operation": "Query",
+          "query": {
+            "expression": "pk = :pk AND begins_with(sk, :sk)",
+            "expressionValues": {
+              ":pk": $util.dynamodb.toDynamoDBJson($ctx.identity.sub),
+              ":sk": $util.dynamodb.toDynamoDBJson("INVOICE#" + $util.defaultIfNullOrEmpty($ctx.args.date, ""))),
+            }
+          }
+        }
+      `),
+      responseMappingTemplate: MappingTemplate.fromString(`
+        $util.toJson($util.defaultIfNullOrEmpty($ctx.result.items, []))
+      `),
+    }));
+
+    api.addQuery('getInvoice', new ResolvableField({
+      dataSource,
+      returnType: invoiceType.attribute(),
+      args: {
+        number: GraphqlType.int({ isRequired: true }),
+      },
+      requestMappingTemplate: MappingTemplate.fromString(`
+        {
+          "version": "2018-05-29",
+          "operation": "GetItem",
+          "key": {
+            "pk": $util.dynamodb.toDynamoDBJson($ctx.identity.sub),
+            "sk": $util.dynamodb.toDynamoDBJson("INVOICE#" + $args.number)
+          }
+        }
+      `),
+      responseMappingTemplate: MappingTemplate.fromString(`
+        $util.toJson($ctx.result.item)
+      `),
+    }));
   }
 }
