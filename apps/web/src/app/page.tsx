c08afe1246f9367916b0212f91431dde71f41610
Martin Trenker <martin@pacabytes.io>
2023-08-29 10:20:42 +0200

    minimap setup

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,113 +1,170 @@
+import React from 'react'
 import Image from 'next/image'
+import clsx from 'clsx';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+import { projects } from './projects';
+
+const Home: React.FC = () => (
+  <main className={clsx([
+      'mx-auto flex flex-col items-center gap-5',
+      'print:m-0 print:mx-14 print:w-full print:max-w-none print:items-start',
+    ])}
+    >
+      <section
+        className={clsx([
+          'prose dark:prose-invert flex-1 px-6',
+          'w-full print:max-w-none print:px-0',
+        ])}
+      >
+        <figure className={clsx([
+          'm-0 text-center',
+          'print:flex print:gap-16 print:text-start',
+        ])}
+        >
+          <picture className={clsx([
+            'inline-block h-[200px] overflow-hidden rounded-full ring ring-zinc-900 dark:ring-zinc-50',
+            'print:h-auto print:rounded-none print:ring-0',
+          ])}
           >
-            By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              alt="Profile picture of Martin Trenker, consultant and web developer"
+              height={200}
+              src="/me.png"
+              width={200}
+              className='m-0'
             />
-          </a>
+          </picture>
+          <figcaption className="flex flex-col gap-4">
+            <h1
+              className={clsx([
+                'm-0 text-3xl font-semibold uppercase',
+              ])}
+            >
+              Martin Trenker
+            </h1>
+            <h2 className={clsx([
+              'm-0 text-2xl uppercase',
+            ])}
+            >
+              Consultant, Web Developer
+            </h2>
+          </figcaption>
+        </figure>
+        <div className={clsx([
+          'print:border-l-2 print:border-zinc-900 print:px-6',
+        ])}
+        >
+          <h3 className="uppercase tracking-widest">About me</h3>
+          <p className="my-1 font-medium tracking-wide">
+            My passion for web development started in the 90s when free web hosting became popular.
+            I became active in the community, helping aspiring developers with HTML, CSS, PHP, MySQL, and JS.
+          </p>
+          <p className="my-1 font-medium tracking-wide">
+            Today, I love building things in the cloud and creating user-oriented,
+            interactive experiences while spreading awareness for clean code and authentic agile practices.
+          </p>
         </div>
+      </section>
+
+      <div className="print:flex print:gap-5">
+        <div className="hidden print:flex print:flex-col print:justify-between print:gap-5">
+          <section
+            className={clsx([
+              'prose dark:prose-invert px-6',
+              'print:max-w-none print:flex-1 print:grow-0 print:px-0',
+            ])}
+          >
+            <h3 className="my-0 uppercase tracking-widest">Contact</h3>
+            <ul className="pl-4">
+              <li className="my-0">info@clean.dev</li>
+              <li className="my-0">https://clean.dev</li>
+            </ul>
+          </section>
+
+          <section
+            className={clsx([
+              'prose dark:prose-invert px-6',
+              'print:max-w-none print:flex-1 print:grow-0 print:px-0',
+            ])}
+          >
+            <h3 className="my-0 uppercase tracking-widest">Skills</h3>
+            <h4 className="uppercase tracking-widest">Programming</h4>
+            <ul className="pl-4">
+              <li className="my-0">Clean Code</li>
+              <li className="my-0">TypeScript</li>
+              <li className="my-0">Serverless</li>
+              <li className="my-0">Web Components</li>
+              <li className="my-0">REST / GraphQL</li>
+            </ul>
+            <h4 className="uppercase tracking-widest">Organizational</h4>
+            <ul className="pl-4">
+              <li className="my-0">Agile Mindset</li>
+              <li className="my-0">Quality Management</li>
+              <li className="my-0">Transparent Communication</li>
+              <li className="my-0">Theory of Constraints</li>
+            </ul>
+          </section>
+        </div>
+
+        <section
+          className={clsx([
+            'prose dark:prose-invert px-6',
+            'print:max-w-none print:flex-1 print:px-0',
+          ])}
+        >
+          <h4 className="uppercase tracking-widest">
+            Clean and Agile
+          </h4>
+          <p className="tracking-wide">
+            I am privileged to have worked with many bright minds over the years and want to share my experience with my clients.
+            With a better understanding of quality management, agile best practices, and a human-centric approach,
+            we can build better products and create a healthier work environment for everyone.
+          </p>
+          <h4 className="uppercase tracking-widest">Learner and Mentor</h4>
+          <p className="tracking-wide">
+            Every project has its unique challenges.
+            I love analyzing and understanding them from a cross-functional perspective to expand my horizon and, on the way, pass some of it to aspiring and seasoned developers alike.
+          </p>
+          <h4 className="uppercase tracking-widest">Automation</h4>
+          <p className="tracking-wide">
+            As an automation nerd, I can help identify and implement automation of repetitive tasks that waste time and energy teams could use for new features instead.
+          </p>
+        </section>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section
+        className={clsx([
+          'prose dark:prose-invert px-6',
+          'print:max-w-none print:px-0',
+        ])}
+      >
+        <h3 className="mt-6 break-before-page uppercase tracking-widest">
+          Projects
+        </h3>
+        {projects.filter(p => p.featured).reverse().map((project) => {
+          const startYear = new Date(project.startDate).getFullYear();
+          const endYear = new Date(project.endDate).getFullYear();
+          return (
+            <article className="my-4 flex flex-col break-inside-avoid" key={project.id}>
+              <div className="flex items-center justify-between gap-6">
+                <h4 className="m-0 flex-initial font-semibold">{project.company ? project.company : project.industry}</h4>
+                <hr className="my-0 block h-px w-full flex-1 border-b border-zinc-700 print:border-zinc-900" />
+                <time dateTime={`${new Date(project.startDate).getFullYear()}`}>
+                  {startYear === endYear ? startYear : `${startYear} - ${endYear}`}
+                </time>
+              </div>
+              <h5 className="m-0 font-semibold uppercase tracking-widest">{project.title}</h5>
+              <p className="m-0 flex-1 tracking-wide">{project.description}</p>
+              <ul className="m-2 pl-4">
+                {project.highlights.length > 0 && project.highlights.map((highlight) => (
+                  <li className="my-0 tracking-wide" key={highlight}>{highlight}</li>
+                ))}
+              </ul>
+            </article>
+          );
+        })}
+      </section>
+  </main>
+);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
