40dd27a2afaebb81f4433466b4404762d68299f6
Martin Trenker <martin@pacabytes.io>
2023-08-30 09:07:48 +0200

    remove old web app

--- packages/clean-web/features/users/hooks/useAuthenticator.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { AuthenticatorContext } from '../components/Authenticator';
-
-export const useAuthenticator = () => {
-  const { setUser, user } = useContext(AuthenticatorContext);
-  return { user, setUser };
-};
