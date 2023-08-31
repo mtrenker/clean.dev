4e45f4ea54e6a269ed97dccde81ba3081f2d0dcf
Martin Trenker <martin@pacabytes.io>
2023-08-31 19:32:07 +0200

    get secret for draft mode

--- apps/web/src/app/api/draft/route.ts
+++ apps/web/src/app/api/draft/route.ts
@@ -1,10 +1,16 @@
 import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
+
+const secretClient = new SecretsManagerClient();
 
 export async function GET(request: Request) {
+  const { SecretString } = await secretClient.send(new GetSecretValueCommand({
+    SecretId: 'DraftModeSecret',
+  }));
   const { searchParams } = new URL(request.url)
   const token = searchParams.get('token');
-  if (token !== 'secret-token') {
+  if (token !== SecretString) {
     return new Response('Invalid token', { status: 401 })
   }
   draftMode().enable()
