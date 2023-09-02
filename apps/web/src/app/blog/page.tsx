1832c2db88a8dd8e0108ac5bd6d009554b9e3f68
Martin Trenker <martin@pacabytes.io>
2023-09-02 11:18:35 +0200

    blog list

--- apps/web/src/app/blog/page.tsx
+++ apps/web/src/app/blog/page.tsx
@@ -5,17 +5,16 @@ import Link from "next/link";
 
 const BlogPage: NextPage = async () => {
   const draft = draftMode();
-  // const posts = await getPosts({ draft: draft.isEnabled });
+  const posts = await getPosts({ draft: draft.isEnabled });
   return (
     <div className="container mx-auto">
-      {/* {posts.map((post) => (
+      {posts.map((post) => (
         <div key={post.slug}>
           <h2>
             <Link href={`/blog/${post.slug}`}>{post.title}</Link>
           </h2>
         </div>
-      ))} */}
-      BlogList
+      ))}
     </div>
   );
 }
