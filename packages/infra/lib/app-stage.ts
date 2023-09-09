283d597b9036261bc44ab47322e9ff7476bd9f3c
Martin Trenker <martin@pacabytes.io>
2023-09-09 12:02:11 +0200

    re-add webstack

--- packages/infra/lib/app-stage.ts
+++ packages/infra/lib/app-stage.ts
@@ -1,5 +1,7 @@
 import { Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { WebStack } from './web-stack';
+import { CertStack } from './cert-stack';
 import { MonitoringStack } from './monitoring-stack';
 
 export class AppStage extends Stage {
@@ -7,5 +9,20 @@ export class AppStage extends Stage {
     super(scope, id, props);
 
     new MonitoringStack(this, 'MonitoringStack', {});
+
+    const certStack = new CertStack(this, 'CertStack', {
+      crossRegionReferences: true,
+      env: {
+        region: 'us-east-1',
+      },
+      domainName: 'clean.dev',
+    });
+
+    const webStack = new WebStack(this, 'WebStack', {
+      crossRegionReferences: true,
+      webCertificateArn: certStack.webCertificate.certificateArn,
+    });
+
+    webStack.addDependency(certStack);
   }
 }
