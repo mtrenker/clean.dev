15d2e28796f88eb0cbc5ceb25d31a6ea033c3d8c
Martin Trenker <martin@pacabytes.io>
2023-09-12 12:51:34 +0200

    fix comment

--- packages/eslint-config/cdk.js
+++ packages/eslint-config/cdk.js
@@ -4,7 +4,7 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /*
  * This is a custom ESLint configuration for use with
- * Next.js apps.
+ * cdk apps.
  *
  * This config extends the Vercel Engineering Style Guide.
  * For more information, see https://github.com/vercel/style-guide
